71036fa803a35a96eccebe7a18d7408593e06299
Petrocini <lucaspetrocinireis@hotmail.com>
2025-04-16 18:38:56 -0300

    fix: use BuildConfig for apiKey

--- app/src/main/java/com/petrocini/movieapp/data/remote/api/MovieApiService.kt
+++ app/src/main/java/com/petrocini/movieapp/data/remote/api/MovieApiService.kt
@@ -1,5 +1,6 @@
 package com.petrocini.movieapp.data.remote.api
 
+import com.petrocini.movieapp.BuildConfig
 import com.petrocini.movieapp.data.remote.dto.MovieDto
 import com.petrocini.movieapp.data.remote.dto.Result
 import com.petrocini.movieapp.utils.TmdbApiConstants
@@ -10,13 +11,13 @@ interface MovieApiService {
 
     @GET(TmdbApiConstants.MOVIE_ENDPOINT)
     suspend fun fetchDiscoverMovie(
-        @Query("api_key") apiKey: String = "",
+        @Query("api_key") apiKey: String = BuildConfig.apiKey,
         @Query("include_adult") includeAdult: Boolean = false
     ): Result
 
     @GET(TmdbApiConstants.TRENDING_MOVIE_ENDPOINT)
     suspend fun fetchTrendingMovie(
-        @Query("api_key") apiKey: String = "",
+        @Query("api_key") apiKey: String = BuildConfig.apiKey,
         @Query("include_adult") includeAdult: Boolean = false
     ): Result
 
